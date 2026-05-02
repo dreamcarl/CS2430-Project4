370855d85c2d04fbe601965d71946a8d89d270b1
dreamcarl <carlosr3cinos@hotmail.com>
2026-05-02 00:55:40 -0600

    Cleaned up directory structure

--- Project4/src/monopoly/SpaceType.java
+++
@@ -1,8 +0,0 @@
-package monopoly;
-/**
- * This class represents the different types of spaces on a monopoly board.
- */
-public enum SpaceType {
-    CHANCE, COMMUNITY_CHEST, FREE_PARKING, GO,
-    JAIL, PROPERTY, RAILROAD, TAX, UTILITY, GO_TO_JAIL;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# CS2430-Project4-Capstone
-https://slcc.instructure.com/courses/1235413/assignments/17763660?module_item_id=31774032
